ec5d88780a97e5789c6b2e86fda2ff465cbe19f2
xohoon <xohoonx2@gmail.com>
2021-12-31 08:41:51 +0900

    devTask project

---
+++ src/main/java/xohoon/devTask/DevTaskApplication.java
@@ -0,0 +1,13 @@
+package xohoon.devTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevTaskApplication.class, args);
+	}
+
+}
