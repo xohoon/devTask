a12f2db9f7bb72098cd391e46d6707ee5ab93bb5
xohoon <xohoonx2@gmail.com>
2022-01-03 03:42:02 +0900

    security : 홈 화면 구성 리뉴얼

--- src/main/java/xohoon/devTask/controller/admin/AdminController.java
+++ src/main/java/xohoon/devTask/controller/admin/AdminController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AdminController {
 
-    @GetMapping(value="/admin")
+    @GetMapping(value="/hide/admin")
     public String home() throws Exception {
         return "admin/home";
     }
