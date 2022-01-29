75ce8a1b1336a01827e426da9595a03dfebd2c72
xohoon <xohoonx2@gmail.com>
2022-01-29 18:56:35 +0900

    main controller set

--- src/main/java/xohoon/devTask/controller/HomeController.java
+++ src/main/java/xohoon/devTask/controller/HomeController.java
@@ -6,16 +6,30 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import xohoon.devTask.domain.entity.Dev;
+import xohoon.devTask.domain.entity.Toy.Toy;
+import xohoon.devTask.domain.entity.task.Task;
 import xohoon.devTask.service.DevService;
+import xohoon.devTask.service.task.TaskService;
+import xohoon.devTask.service.toy.ToyService;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
 
+    private final TaskService taskService;
+    private final ToyService toyService;
     private final DevService devService;
 
     @GetMapping(value = {"/", "/home", "/index"})
-    public String home() {
+    public String home(Model model) {
+        List<Task> tasks = taskService.getTasks();
+        List<Toy> toys = toyService.getToys();
+
+        model.addAttribute("tasks", tasks);
+        model.addAttribute("toys", toys);
+
         return "home";
     }
 
